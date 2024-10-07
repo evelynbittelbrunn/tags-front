0fbbc786c5ad02af915396a18352ca8f9d13cdd9
Evelyn Bittelbrunn <74337638+evelynbittelbrunn@users.noreply.github.com>
2024-10-07 01:33:22 -0300

    Iniciando o desenvolvimento do perfil do usuário

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 import './App.css';
 import Feed from './pages/feed/Feed';
 import NavBar from './components/navBar/NavBar';
+import Profile from './pages/profile/Profile';
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
                 <div className='main-container'>
                     <Routes>
                         <Route path="/" element={<Feed />} />
+                        <Route path="/perfil" element={<Profile />} />
                     </Routes>
                 </div>
             </BrowserRouter>
