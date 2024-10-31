31fc22dc756a97a613d1317cdb9d16ff69cf6fb8
Evelyn Bittelbrunn <74337638+evelynbittelbrunn@users.noreply.github.com>
2024-10-30 22:40:24 -0300

    Carregando o conteúdo da postagem (incluindo imagem)

--- src/components/postFeed/components/Page.tsx
+++ src/components/postFeed/components/Page.tsx
@@ -5,6 +5,7 @@ import { POSTS_GET } from '../../../services/api';
 import Heart from '../../icons/Heart';
 import Comment from '../../icons/Comment';
 import CommentsModal from './CommentsModal';
+import { PostAttributes } from '../IPostFeed';
 
 interface IPage {
     infinite: boolean;
@@ -42,19 +43,28 @@ const Page = ({
 
     if (posts.length > 0) return (
         <>
-            {posts.map(post => {
+            {posts.map((post: PostAttributes) => {
+
+                const { user } = post;
+
                 return (
                     <>
                         <div className='card-post-feed'>
                             <span>
                                 <Avatar style={{ backgroundColor: '#87d068' }} icon={<UserOutlined />} />
                                 <div>
-                                    <h4>Alice de Oliveira Gonçalves Amaranto</h4>
+                                    <h4>{user.name}</h4>
                                     <span>Há 3 horas</span>
                                 </div>
                             </span>
-                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-                            <p>It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                            <p>{post.content}</p>
+                            {post.imageData && (
+                                <img
+                                    src={`data:image/jpeg;base64,${post.imageData}`}
+                                    alt="Post Image"
+                                    style={{ maxWidth: '100%', height: 'auto' }}
+                                />
+                            )}
                             <div className='post-actions'>
                                 <Heart />
                                 <Comment setOpen={setOpenCommentsModal} />
