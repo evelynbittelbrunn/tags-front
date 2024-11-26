7d2d5e55e4d5fdd9867e60ec2db9fa19c1725ddc
Evelyn Bittelbrunn <74337638+evelynbittelbrunn@users.noreply.github.com>
2024-11-25 23:37:49 -0300

    Ajustes na navbar

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,6 +1,6 @@
 import { HomeOutlined, UserOutlined } from '@ant-design/icons';
 import { Avatar, Button, Popover } from 'antd'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import './styles.css';
 import { useContext } from 'react';
 import { UserContext } from '../../contexts/UserContext';
@@ -9,6 +9,7 @@ import SearchAccounts from './searchAccounts/SearchAccounts';
 export default function NavBar() {
 
     const { userLogout } = useContext(UserContext);
+    const { pathname } = useLocation();
 
     const content = (
         <div style={{ display: "flex", flexDirection: "column" }}>
@@ -17,11 +18,10 @@ export default function NavBar() {
         </div>
     );
 
-
     return (
         <div className='navbar-container'>
             <nav>
-                <Link to="/" style={{ padding: '0 8px' }}><HomeOutlined /></Link>
+                <Link to="/" className={`feed-nav-button ${pathname === '/' && 'selected'}`}><HomeOutlined /></Link>
                 <SearchAccounts />
                 <Popover content={content}>
                     <Avatar icon={<UserOutlined />} />
